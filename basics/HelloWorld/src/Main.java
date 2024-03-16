2fa33633ddcca7df00f0b57105d755ce18af9926
Nahid Hossen <nahid957hossen@gmail.com>
2024-03-17 00:20:10 +0600

    update new project

--- basics/HelloWorld/src/Main.java
+++ basics/HelloWorld/src/Main.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i < ; i++) {
+            System.out.println("hello nahid");
+        }
     }
 }
