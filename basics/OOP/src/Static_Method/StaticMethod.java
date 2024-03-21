36768385a01e951d28b7349b98e1db7a9387484c
Nahid Hossen <nahid957hossen@gmail.com>
2024-03-21 16:57:32 +0600

    Static_mehtod

---
+++ basics/OOP/src/Static_Method/StaticMethod.java
@@ -0,0 +1,10 @@
+package Static_Method;
+
+public class StaticMethod {
+    void display1(){
+        System.out.println("I am Non Static Method");
+    }
+    static void display2(){
+        System.out.println("I am Static Method");
+    }
+}
